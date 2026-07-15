1ad59434a3174da805f174cad6640918a6755346
Fatimaeifh <fatimaesky85@gmail.com>
2026-07-15 20:10:27 +0330

    Create Day01.py

---
+++ Day01.py
@@ -0,0 +1,5 @@
+yourName=str(input("Enter your name :"))
+yourAge=int(input("Enter your age:"))
+yourLength=float(input("Enter your length:"))
+
+print(f"I'm {yourName} and I'm {yourAge} years old and my length is {yourLength}")
