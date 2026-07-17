9c38e7d8c2d4d2a960f2f34733b2030706fe59a7
Fatimaeifh <fatimaesky85@gmail.com>
2026-07-17 18:33:21 +0330

    loop practices

---
+++ Day03.py
@@ -0,0 +1,39 @@
+#practice 1: print numbers 1 to 10
+#for x in range(1,11):
+#    print(x)
+#======================================
+#practice 2:print even numbers 2 to 100
+#for x in range(2,101,2):
+#    print(x)'''
+#===========
+#for x in range(1,101):
+#    if x%2==0:
+#       print(x)
+#======================================
+#practice 3: print odd numbers 1 to 50
+#for x in range(1,51,2):
+#    print(x)
+#===========
+#for x in range(1,51):
+#    if x%2==1:
+#        print(x)
+#======================================
+#practice 4: the sum of numbers 1 to 100
+#sum=0
+#for x in range(1,101):
+#    sum+=x
+#print(sum)
+#======================================
+#practice 5: Multiplication table
+#n=int(input("Enter a number:"))
+#for x in range(1,n+1):
+#    print(f"{n} * {x} = {n*x}")
+#======================================
+#practice 6 
+#Mypass="1234"
+#Yourpass=str(input("Please Enter your password: "))
+#while  Mypass!=Yourpass:
+#    yourpass=int(input("Wrong! Please Enter your password again: "))
+#else:
+#    print("Welcome!")
+
