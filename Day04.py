89b2f2294a7da98f99cc14234e7b6f351c43a22c
Fatimaeifh <fatimaesky85@gmail.com>
2026-07-18 14:13:44 +0330

    Functions practices

---
+++ Day04.py
@@ -0,0 +1,72 @@
+#Practice 1 :print hello name with Function
+#def MyName(name):
+#    print("Hello",name)
+
+#name=str(input("Please Enter your name: "))
+#MyName(name)
+#====================================
+#Practise 2 : add to number with Function
+#def Add(x,y):
+#    return x+y
+#num1=int(input("Please Enter first number:"))
+#num2=int(input("Please Enter seceond number:"))
+#print(Add(num1,num2))
+#====================================
+#Practice 3 : The number is even or odd with Function
+#def odd_even(number):
+#if number%2==0:
+#       return "Even"
+#    else:
+#        return "Odd"
+    
+#number=int(input("Enter your number please: "))
+#print(odd_even(number))
+#====================================
+#Practice 4 : compare 2 numbers and return bigger number with Function
+#def Bignumber(x,y):
+#    if x>y:
+#        return x
+#    elif x<y:
+#        return y
+#    else:
+#        return "x and y are equal."
+    
+
+#print(Bignumber(num1,num2))
+#====================================
+#Practice 5 : Describe Grades with alphabet with Function
+#def DescribeGrade(Grade):
+#    if Grade>90:
+#        return "A"
+#    elif Grade>80:
+#        return "B"
+#    else:
+#        return "C"
+#grade=float(input('Enter your Grade please:'))
+#print(DescribeGrade(grade))
+#====================================
+#Practice 6 : culcalate Area with Function
+#def Area(x,y):
+#    return x*y
+#length=int(input("Please Enter length:"))
+#width=int(input("Please Enter width:"))
+#print(Area(length,width))
+#===================================
+#Practice 7:  Calculator with Functions
+#def calculator(x,y,operator):
+#    if operator=="+":
+#        return x+y
+#    elif operator=="-":
+#        return x-y
+#    elif operator=="*":
+#        return x*y
+#    elif operator=="/":
+#        return x/y
+#    else:
+#        return "operator not found!"
+
+#num1=int(input("Please Enter first number:"))
+#num2=int(input("Please Enter seceond number:"))
+#operator=input("Please Enter your operator:(+,-,*,/)")
+#print(calculator(num1,num2,operator))
+#====================================
